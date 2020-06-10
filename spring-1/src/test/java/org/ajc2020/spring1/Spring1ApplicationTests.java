872b3dcba060d046a98c4f825b177e0bc1c58e3e
Kornél <stefankornel02@gmail.com>
2020-06-10 13:51:08 +0200

    Initial commit

---
+++ spring-1/src/test/java/org/ajc2020/spring1/Spring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajc2020.spring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
