b036c5a5137dfa912ab2ef90ed61d03d43f110f8
Kornél <stefankornel02@gmail.com>
2020-06-15 16:28:58 +0200

    Equals fix (?)

--- spring-1/src/main/java/org/ajc2020/spring1/model/Ticket.java
+++ spring-1/src/main/java/org/ajc2020/spring1/model/Ticket.java
@@ -26,17 +26,4 @@ public class Ticket {
     @Accessors(chain = true)
     @JsonBackReference
     private Worker worker;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ticket ticket = (Ticket) o;
-        return id == ticket.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
