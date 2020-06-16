aa803689253fd27ef7a193ba9bfedefe2cff73a3
Kornél <stefankornel02@gmail.com>
2020-06-16 20:20:39 +0200

    Promo images Translation fix

--- worker-ui/src/general/Footer.tsx
+++ worker-ui/src/general/Footer.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import style from '../resource/style/footer.module.css';
 import logo from '../resource/img/logo.png';
+import {useTranslation} from "react-i18next";
 
 const Footer: React.FunctionComponent<{
     handleLogout: () => void
 }> = (props => {
+    const {t} = useTranslation();
+
     return (
         <div className={style.Logout}>
-            <a onClick={props.handleLogout} href="#logout">Kijelentkezés</a>
+            <a onClick={props.handleLogout} href="#logout">{t("footer.logout")}</a>
             <img src={logo} alt="logo" className={style.LogoutImage}/>
             <small>
                 KIBe {new Date().getFullYear()} &copy;
