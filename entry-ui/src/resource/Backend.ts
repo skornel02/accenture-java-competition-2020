377cf25ba3e2733dce5d59a3205eae3be778604a
Kornél <stefankornel02@gmail.com>
2020-06-26 15:05:31 +0200

    Entry-ui init

---
+++ entry-ui/src/resource/Backend.ts
@@ -0,0 +1,64 @@
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig} from 'axios/index';
+import {AuthenticationInformation} from "./Resources";
+
+const settingsProduction = {
+    url: "http://localhost:8080",
+};
+
+const settingsDevelopment = {
+    url: "http://localhost:8080"
+};
+
+const ADDR: string = process.env.NODE_ENV === 'development' ? settingsDevelopment.url : settingsProduction.url;
+
+const axiosBaseConfig: AxiosRequestConfig = {
+    baseURL: ADDR,
+    timeout: 8000,
+    timeoutErrorMessage: "Server timeout",
+    headers: {
+        "Content-Type": "application/json"
+    }
+};
+
+class Backend {
+    private axios: AxiosInstance;
+
+    constructor() {
+        this.axios = axios.create({
+            ...axiosBaseConfig
+        });
+    }
+
+    updateAuthentication(deviceToken: string) {
+        this.axios = axios.create({
+            ...axiosBaseConfig,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "DeviceToken " + deviceToken,
+            }
+        });
+    }
+
+    async checkDeviceToken(deviceToken: string): Promise<AuthenticationInformation> {
+        try {
+            const result = await this.axios.get<AuthenticationInformation>("/auth-information", {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "DeviceToken " + deviceToken,
+                }
+            });
+            return result.data;
+        } catch (err) {
+            if (err) {
+                const axiosError = err as AxiosError;
+                if (err.response) {
+                    console.error(axiosError.response);
+                }
+            }
+            throw err;
+        }
+    }
+
+}
+
+export default new Backend();
---
+++ entry-ui/src/resource/Resources.ts
@@ -0,0 +1,73 @@
+export enum AuthenticationType {
+    NONE,
+    BASIC,
+    GOOGLE
+}
+
+export interface Authentication {
+    type: AuthenticationType
+}
+
+export interface BasicAuthentication extends Authentication {
+    type: AuthenticationType.BASIC,
+    username: string,
+    password: string,
+}
+
+export interface GoogleAuthentication extends Authentication {
+    type: AuthenticationType.GOOGLE,
+    googleToken: string
+}
+
+export interface BasicLoginInformation {
+    email: string,
+    password: string
+}
+
+export type PermissionLevel = "WORKER" | "ADMIN" | "SUPER_ADMIN" | "DEVICE" | "INVALID";
+
+export type WorkerStatus = "WorkingFromHome" | "OnList" | "InOffice";
+
+export interface WorkerResource {
+    id: string,
+    name: string,
+    email: string,
+    rfId: string,
+    averageTime: number,
+    status: WorkerStatus
+}
+
+export interface AdminResource {
+    uuid: string,
+    name: string,
+    email: string,
+}
+
+export interface AuthenticationInformation {
+    permission: PermissionLevel,
+    worker?: WorkerResource,
+    admin?: AdminResource
+}
+
+export interface LoginResource {
+    id: number,
+    arrivedAt: string,
+    leftAt: string,
+    worker: WorkerResource
+}
+
+export interface OfficeHoursResource {
+    arrive: string,
+    leave: string
+}
+
+export interface RemainingTime {
+    projectedEntryTime: string,
+    status: WorkerStatus,
+    locationSVG: string,
+}
+
+export interface TicketResource {
+    targetDay: string,
+    creationDate: string
+}
