f51ba360fe892e4d6009ded9f63fe561ab730fb0
Kornél <stefankornel02@gmail.com>
2020-06-27 20:08:08 +0200

    entry-ui SVGs

--- entry-ui/src/component/MainComponent.tsx
+++ entry-ui/src/component/MainComponent.tsx
@@ -4,16 +4,25 @@ import SvgDisplay from './SvgDisplay';
 import {useTranslation} from "react-i18next";
 import Backend from "../resource/Backend";
 import {toast} from "react-toastify";
+import {
+    alreadyInsideSvg,
+    alreadyOutsideSvg,
+    cantEnter,
+    errorSvg,
+    idleSvg,
+    leaveSvg,
+    notFoundSvg
+} from "../resource/img/DefaultSvgs";
 
 const MainComponent: React.FunctionComponent = () => {
-    const [mapSvg, setMapSvg] = useState<string | undefined>(undefined);
+    const [mapSvg, setMapSvg] = useState<string | undefined>(idleSvg);
     const [rfid, setRfid] = useState<string>("");
     const {t} = useTranslation();
     useEffect(() => {
         let timeout: NodeJS.Timeout | undefined = undefined;
-        if (mapSvg !== undefined) {
+        if (mapSvg !== idleSvg) {
             timeout = setTimeout(() => {
-                setMapSvg(undefined);
+                setMapSvg(idleSvg);
             }, 8000);
         }
         return () => {
@@ -30,15 +39,19 @@ const MainComponent: React.FunctionComponent = () => {
                 switch (result.status) {
                     case "ERROR":
                         toast("Unrecognized error", {type: "error"});
+                        setMapSvg(errorSvg);
                         break;
                     case "FULL_HOUSE":
                         toast(t("rfid.fullHouse"), {type: "error"});
+                        setMapSvg(cantEnter);
                         break;
                     case "UNKNOWN_RF_ID":
                         toast(t("rfid.rfidNotFound"), {type: "error"});
+                        setMapSvg(notFoundSvg);
                         break;
                     case "NOT_OUTSIDE":
                         toast(t("rfid.alreadyInside"), {type: "info"});
+                        setMapSvg(alreadyInsideSvg);
                         break;
                     case "OK":
                         toast(t("rfid.youCanEnter"), {type: "success"})
@@ -60,18 +73,19 @@ const MainComponent: React.FunctionComponent = () => {
                 switch (result.status) {
                     case "ERROR":
                         toast("Unrecognized error", {type: "error"});
-                        break;
-                    case "FULL_HOUSE":
-                        toast(t("rfid.fullHouse"), {type: "error"});
+                        setMapSvg(errorSvg);
                         break;
                     case "UNKNOWN_RF_ID":
                         toast(t("rfid.rfidNotFound"), {type: "error"});
+                        setMapSvg(notFoundSvg);
                         break;
                     case "NOT_INSIDE":
                         toast(t("rfid.alreadyOutside"), {type: "info"});
+                        setMapSvg(alreadyOutsideSvg);
                         break;
                     case "OK":
                         toast(t("rfid.youCanLeave"), {type: "success"})
+                        setMapSvg(leaveSvg);
                         break;
                 }
             })
